f7bb7249ae137f1aa7331c3e8913ee8c9dad9325
Vadim <vadimkurb22@gmail.com>
2024-12-01 13:46:10 +0300

    01.12.24 post and get done 40:30

--- server/controllers/busController.js
+++ server/controllers/busController.js
@@ -3,17 +3,22 @@ const ApiError = require('../error/ApiError')
 
 class BusController {
     async create(req, res) {
-        const {model} = req.body
+        try {
+            const {serial_num, model, type, year, seats} = req.body
 
                
-        const bus = await Bus.create({model})
-        
-        return res.json(bus)
-
+        const bus = await Bus.create({serial_num, model, type, year, seats})
+            return res.json(bus);
+        } catch (error) {
+            console.error('Error creating flight:', error);
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
     }
 
     async getAll(req, res) {
-        return res.json({ message: 'Все работает' });
+        //return res.json({ message: 'Все работает' });
+        const buses = await Bus.findAll()
+        return res.json(buses)
     }
 }
 

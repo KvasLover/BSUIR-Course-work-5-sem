1e2dcc1611245649a1ffcfff9dda77bcd2469e34
Vadim <vadimkurb22@gmail.com>
2024-12-01 16:21:39 +0300

    01.12.24 first filter (showAll tickets) 51:21

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -1,18 +1,44 @@
-/*const {Flight, Ticket} = require('../models/models')
-const ApiError = require('../error/ApiError')*/
+const {Ticket} = require('../models/models')
+const ApiError = require('../error/ApiError')
 const uuid = require('uuid')
 const path = require('path')
 
 class TicketController {
-    async create(req, res) {
-        const {flight_id, seat_number, ticket_status} = req.body
-        const {img} = req.files
-        let fileName = uuid.v4() + ".jpg"
-        img.mv(path.resolve(__dirname, '..', 'static', fileName))
+    
+    async create(req, res, next) {
+        try {
+            const {flight_id, seat_number, ticket_status} = req.body
+            const {img} = req.files
+            let fileName = uuid.v4() + ".jpg"
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+            
+            const ticket = await Ticket.create({
+                flight_id, seat_number, ticket_status, img: fileName
+            })
+            
+            return res.json(ticket)
+        } catch(e) {
+            next(ApiError.badRequest(e.message))
+        }
     }
 
+    // С возможностью фильтра по рейсам и задания
+    // лимита билетов на странице и номера текущей страницы
     async getAll(req, res) {
-        const tickets = await Ticket.findAll()
+        let {flight_id, limit, page} = req.query
+        page = page || 1
+        limit = limit || 9
+        let offset = page * limit - limit
+        let tickets
+        if (!flight_id) {
+            tickets = await Ticket.findAndCountAll({limit, offset})
+        }
+        if (flight_id) {
+            tickets = await Ticket.findAndCountAll({where: {flight_id}, limit, offset})
+        }
+        
+       //const tickets = await Ticket.findAll()
+        
         return res.json(tickets)
     }
 
